2085ec59f955648852bbb1245f7f6bac7fec6c52
Meda Girish kumar <girish573kumar@gmail.com>
2024-10-26 04:23:55 +0530

    Added Certificates section to portfolio

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './components/About';
 import Skills from './components/Skills';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
+import Certificates from './components/Certificates';
 import Languages from './components/Languages';
 import Contact from './components/Contact';
 import './App.css';
@@ -16,6 +17,7 @@ function App() {
       <Skills />
       <Experience />
       <Projects />
+      <Certificates></Certificates>
       <Languages></Languages>
       <Contact />
     </div>
