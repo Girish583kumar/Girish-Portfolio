d8aca9ae23e6808fd7df66ae25e914afbd547f7e
Meda Girish kumar <girish573kumar@gmail.com>
2024-10-29 14:24:46 +0530

    Update portfolio with latest changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,21 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {  faGithub, } from '@fortawesome/free-brands-svg-icons';
 import './Header.css'; // Make sure this path is correct
+//import pic from "./Profile.jpg";//
+
+
 
 const Header = () => {
     return (
         <header className="header">
             <div className="profile">
                 <img
-                    src='C:/Users/Meda Sesha priyanka/Downloads/data-analyst-portfolio/public/assets/Girish-Profile-pic.jpg' // Replace this with your actual profile image path
+                    src="C:/Users/Meda Sesha priyanka/Downloads/data-analyst-portfolio/src/components/Profile.jpg"// Replace this with your actual profile image path
                     alt="Profile"
                     className="profile-pic"
                 />
                 <h1 className="profile-name">Meda Girish Kumar</h1>
+                <h1 className="role">Data Analyst/Power BI Developer/Tableau Developer</h1> {/* Add the role here */}
                 <h1 className="profile-name">girish573kumar@gmail.com</h1>
                 <h1 className="profile-name">6302737269</h1>
                 
